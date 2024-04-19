ae5a3ed3b39c95b0e0453ae5069c367ec38e5a67
octodaniil <dan.khegai@gmail.com>
2024-04-19 21:06:36 +0300

    Структура 3 трай

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
+// Интерфейс модели товара
 interface IProduct {
   id: string;
   title: string;
@@ -5,25 +6,15 @@ interface IProduct {
   image: string;
   category: string;
   price: number;
-
-  addProductToBasket(productId: string): void;
-  removeProductToBasket(productId: string): void;
 }
 
+// Интерфейс модели списка товаров
 interface IProductList {
   total: number;
   items: IProduct[];
-
-  getProduct(productId: string): IProduct;
 }
 
-interface IBasket {
-  total: number;
-  items: IProduct[];
-
-  checkout(): void;
-}
-
+// Интерфейс модели заказа
 interface IOrder {
   payment: string;
   email: string;
@@ -31,4 +22,60 @@ interface IOrder {
   address: string;
   total: number;
   items: IProduct[];
+}
+
+// Презентер товара
+interface IProductPresenter {
+  // Получить список товаров
+  getProductList(): void;
+  // Получить детали о товаре по его ID
+  getProductDetails(productId: string): void;
+}
+
+// Презентер корзины
+interface ICartPresenter {
+  // Добавить товар в корзину
+  addToCart(product: IProduct): void;
+  // Удалить товар из корзины по его ID
+  removeFromCart(productId: string): void;
+  // Оформить заказ
+  checkout(order: IOrder): void;
+}
+
+// Презентер оформления заказа
+interface IOrderPresenter {
+  // Отправить заказ на сервер
+  submitOrder(order: IOrder): void;
+}
+
+// Представление каталога товаров
+interface IProductCatalogView {
+  // Отобразить список товаров
+  showProductList(productList: IProductList): void;
+  // Отобразить детали о товаре
+  showProductDetails(product: IProduct): void;
+}
+
+// Представление корзины
+interface ICartView {
+  // Отобразить товары в корзине
+  showCartItems(order: IOrder): void;
+  // Показать окно оформления заказа
+  showOrderPopup(): void;
+}
+
+// Представление оформления заказа
+interface IOrderPopupView {
+  // Показать доступные способы оплаты
+  showPaymentMethods(): void;
+  // Показать форму для ввода информации о заказе
+  showOrderForm(): void;
+  // Получить детали о заказе из формы
+  getOrderDetails(): IOrder;
+}
+
+// Представление подтверждения заказа
+interface IOrderConfirmationPopupView {
+  // Показать сообщение о подтверждении заказа
+  showConfirmationMessage(): void;
 }
